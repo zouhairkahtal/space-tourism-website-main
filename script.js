9f709aff0a8023032a59c15621e86251194d8262
zouhair <mizouhair4@gmail.com>
2025-01-14 21:36:26 +0100

    add  destination.html

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const openbtn = document.getElementById("open");
 const closebtn = document.getElementById("close");
 const nav = document.getElementById("nav");
 const navd = document.getElementById("navd");
+const home = document.getElementById("home");
 
 openbtn.addEventListener("click", () => {
   openbtn.style.display = "none";
